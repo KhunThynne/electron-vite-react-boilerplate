02d04341b56317aeaf3cf9feffd91d462f980f3c
Khunthynne <khunthynne@gmail.com>
2026-01-18 02:34:25 +0700

    chore: implement ContainerSection and change crlf to lf some file

--- src/renderer/src/provider.tsx
+++ src/renderer/src/provider.tsx
@@ -2,14 +2,17 @@
 import { ThemeProvider } from 'next-themes'
 import { Toaster } from '@components/ui/sonner'
 import { SidebarProvider } from './shared/contexts/ProviderSidebar'
+import { DialogProvider } from './shared/libs/dialog/DialogProvider'
 
 export default function Provider({ children }: { children: React.ReactNode }): React.JSX.Element {
   return (
     <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-      <SidebarProvider>
-        <Toaster />
-        {children}
-      </SidebarProvider>
+      <DialogProvider>
+        <SidebarProvider>
+          <Toaster />
+          {children}
+        </SidebarProvider>
+      </DialogProvider>
     </ThemeProvider>
   )
 }
